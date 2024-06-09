2f6d89dbb6068da89b6d962e1233f105ec1a269f
iskipschoolforthis <vex0901@gmail.com>
2024-06-09 16:55:36 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,35 +1,62 @@
 #!/bin/bash
 
-# Function to fetch the package tarball, build, and install
-fetch_build_install() {
-    url="$example1"
-    build_script="$example2"
+# Function to get the package
+get_package() {
+    url="$1"
+    wget "$url" -O package_download
+}
 
-    # Fetch package tarball
-    echo "Fetching package from $url..."
-    wget "$url" -O package.tar.gz
+# Function to extract the package
+extract_package() {
+    file_type=$(file package_download | awk '{print $2}')
+    case "$file_type" in
+        "gzip")
+            tar -xzf package_download
+            ;;
+        "bzip2")
+            tar -xjf package_download
+            ;;
+        "Zip")
+            unzip package_download
+            ;;
+        "XZ")
+            tar -xJf package_download
+            ;;
+        "POSIX")
+            tar -xf package_download
+            ;;
+        *)
+            echo "Unsupported package format: $file_type"
+            exit 1
+            ;;
+    esac
+}
 
-    # Run build script
-    if [ -x "$build_script" ]; then
-        echo "Running build script..."
-        sh "$build_script"
-    else
-        echo "Build script not found."
-        exit 1
-    fi
+# Function to build and install the package
+build_install_package() {
+    package_dir=$(tar -tf package_download | head -1 | cut -f1 -d"/")
+    cd "$package_dir" || exit
+    ./configure
+    make
+    make install
+}
+
+# Function to uninstall the package
+uninstall_package() {
+    package_dir=$(tar -tf package_download | head -1 | cut -f1 -d"/")
+    cd "$package_dir" || exit
+    make uninstall
 }
 
 # Main function
 main() {
-    if [ $# -ne 2 ]; then
-        echo "Usage: $0 [url] [build_script]"
-        exit 1
+    if [ "$1" == "--uninstall" ]; then
+        uninstall_package
+    else
+        get_package "$1"
+        extract_package
+        build_install_package
     fi
-    url="$1"
-    build_script="$2"
-    fetch_build_install "$url" "$build_script"
 }
 
-# Execute main function
 main "$@"
-
